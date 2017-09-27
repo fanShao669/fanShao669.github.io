5761f0529fa02442ee17856dd7e302b534e7824c
zhaoxiaofan <18301545298@163.com>
2017-09-27 16:12:37 +0800

    update

--- vue/fivezreofive/webpack.config.js
+++ vue/fivezreofive/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './static'),
-    publicPath: '/static/',
+    path: path.resolve(__dirname, 'static'),
+    publicPath: 'static/',
     filename: 'build.[hash].js'
   },
   module: {
